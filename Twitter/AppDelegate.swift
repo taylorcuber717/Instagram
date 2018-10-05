8b879ae5c0803c3b43e7dca8a190c478ce29fc75
Taylor McLaughlin <taylormclaughlin@dhcp-10-134-193-224.uawifi.arizona.edu>
2018-10-05 14:21:09 -0700

    Posts view controller not working

--- Twitter/AppDelegate.swift
+++ Twitter/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.initialize(
+            with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "codepathTwitter"
+                configuration.clientKey = "alaksdfhlkjasdhfljkadshflkjasdhfjklahsdfeiu"
+                configuration.server = "https://fast-ravine-77564.herokuapp.com/parse"
+            })
+        )
+        NotificationCenter.default.addObserver(forName: Notification.Name("didLogout"), object: nil, queue: OperationQueue.main) { (Notification) in
+            print("Logout notification received")
+            // TODO: Logout the User
+            // TODO: Load and show the login view controller
+        }
+        if PFUser.current() != nil {
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            // view controller currently being set in Storyboard as default will be overridden
+            window?.rootViewController = storyboard.instantiateViewController(withIdentifier: "InstagramTabBarController")
+        }
         return true
     }
 
---
+++ Twitter/PostsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  PostsViewController.swift
+//  
+//
+//  Created by Taylor McLaughlin on 10/4/18.
+//
+
+import UIKit
+
+class PostsViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var takePhotoLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
